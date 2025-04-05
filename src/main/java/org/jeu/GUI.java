dff6c9cf954f8269da67d7bc5a683ebf5496987c
Maxime Vernoux <119664725+IMxMI@users.noreply.github.com>
2025-04-05 16:37:56 +0200

    refactor(GUI) : Changement fichier source

--- src/main/java/org/jeu/GUI.java
+++ src/main/java/org/jeu/GUI.java
@@ -1,4 +1,4 @@
-package org.example;
+package org.jeu;
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
@@ -27,10 +27,13 @@ public class GUI implements ActionListener
     }
 
    public void afficheImage( String nomImage) {
-	   	URL imageURL = this.getClass().getClassLoader().getResource("jeu/images/" + nomImage);
+	   	URL imageURL = this.getClass().getClassLoader().getResource("images/" + nomImage);
 	   	if( imageURL != null ) {
         	image.setIcon( new ImageIcon( imageURL ));
             fenetre.pack();
+        }
+           else{
+            System.err.println("Image non trouvée " + nomImage);
         }
    }
 
