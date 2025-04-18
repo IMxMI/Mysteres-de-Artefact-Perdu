6b75955db2ab78e1c9b5938cdb72c96cebdcc7ea
mathiascazals <cazalsmathias@gmail.com>
2025-04-18 16:59:55 +0200

    Feat : Add features on Sac class

--- src/main/java/org/jeu/Sac.java
+++ src/main/java/org/jeu/Sac.java
@@ -5,17 +5,23 @@ import java.util.List;
 
 public class Sac {
     private List<Item> items;
+    private static final int CAPACITE_MAX = 5;
 
     public Sac() {
         items = new ArrayList<>();
     }
 
     public boolean ajouterItem(Item item) {
-
-        return items.add(item);
+        if (items.size() >= CAPACITE_MAX) {
+            System.out.println("Le sac est plein ! \n Vous ne pouvez pas porter plus de " + CAPACITE_MAX + " objets.");
+            return false;
+        }
+        else {
+            return items.add(item);
+        }
     }
 
-    public boolean prendreItem(Item item) {
+    public boolean enleverItem(Item item) {
 
         return items.remove(item);
     }
